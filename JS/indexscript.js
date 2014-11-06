0f8d0c2f8e275082b7312562006fb7784907c3ea
Ian osawaye <Osasosawaye44@yahoo.com>
2014-11-06 16:57:41 -0500

    Implemented pic and aboutme profile

--- JS/indexscript.js
+++ JS/indexscript.js
@@ -1,5 +1,11 @@
 $(document).ready(function()
 	{
+    $("h1").slideDown(1000,showh2());
+
+    function showh2(){
+      $("h2").slideDown(2000);
+    }
+
 		$(".btn, .btn-facebook").mouseenter(
     function()
     {
@@ -13,9 +19,5 @@ $(document).ready(function()
        $(this).attr("id","special-buttons-color");
         $(this).animate({bottom:"0px"},400);}
      );
-
-
-
-       var curScroll = 0;
 
 	});
